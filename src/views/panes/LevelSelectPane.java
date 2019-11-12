09b49c1801210597e929e0112bf9e8059bf464d0
Jordan Lee <jordanlee80@gmail.com>
2019-11-12 18:29:48 +0800

    init

---
+++ src/views/panes/LevelSelectPane.java
@@ -0,0 +1,124 @@
+package views.panes;
+
+import controllers.LevelManager;
+import controllers.SceneManager;
+import io.Deserializer;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ListView;
+import javafx.stage.DirectoryChooser;
+import models.FXGame;
+import views.BigButton;
+import views.BigVBox;
+import views.SideMenuVBox;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class LevelSelectPane extends GamePane {
+
+    private SideMenuVBox leftContainer = new SideMenuVBox();
+    private BigButton returnButton = new BigButton("Return");
+    private BigButton playButton = new BigButton("Play");
+    private BigButton playRandom = new BigButton("Generate Map and Play");
+    private BigButton chooseMapDirButton = new BigButton("Choose map directory");
+    private ListView<String> levelsListView = new ListView<>(LevelManager.getInstance().getLevelNames());
+    private BigVBox centerContainer = new BigVBox();
+    private Canvas levelPreview = new Canvas();
+
+    public LevelSelectPane() {
+        connectComponents();
+        styleComponents();
+        setCallbacks();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void connectComponents() {
+        // TODO
+        leftContainer.getChildren().add(returnButton);
+        leftContainer.getChildren().add(chooseMapDirButton);
+        leftContainer.getChildren().add(levelsListView);
+        leftContainer.getChildren().add(playButton);
+        leftContainer.getChildren().add(playRandom);
+        this.setLeft(leftContainer);
+        centerContainer.getChildren().add(levelPreview);
+        this.setCenter(centerContainer);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void styleComponents() {
+        // TODO
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void setCallbacks() {
+        // TODO
+        returnButton.setOnAction(null);
+        chooseMapDirButton.setOnAction((e)->promptUserForMapDirectory());
+        playButton.setOnAction((e)->startGame(false));
+        playRandom.setOnAction((e)->startGame(true));
+    }
+
+    /**
+     * Starts the game.
+     *
+     * <p>
+     * This method should do everything that is required to initialize and start the game, including loading/generating
+     * maps, switching scenes, etc.
+     * </p>
+     *
+     * @param generateRandom Whether to use a generated map.
+     */
+    private void startGame(final boolean generateRandom) {
+        // TODO
+        if(generateRandom){
+
+        }
+        else{
+
+        }
+    }
+
+    /**
+     * Listener method that executes when a map on the list is selected.
+     *
+     * @param observable Observable value.
+     * @param oldValue   Original value.
+     * @param newValue   New value.
+     */
+    private void onMapSelected(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        // TODO
+    }
+
+    /**
+     * Prompts the user for a map directory.
+     *
+     * <p>
+     * Hint:
+     * Use {@link DirectoryChooser} to display a folder selection prompt.
+     * </p>
+     */
+    private void promptUserForMapDirectory() {
+        // TODO
+        DirectoryChooser d = new DirectoryChooser();
+        commitMapDirectoryChange(d.getInitialDirectory());
+    }
+
+    /**
+     * Actually changes the current map directory.
+     *
+     * @param dir New directory to change to.
+     */
+    private void commitMapDirectoryChange(File dir) {
+        // TODO
+    }
+}
